4fb151196705259309bc242bc24386cdc60dfeb5
João Luiz Lima Nogueira <devjoaoluiz@gmail.com>
2024-01-25 15:34:12 -0300

    esc button function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 createBtn.onclick = () => {
-    modal.classList.add("active"); 
-    overlay.classList.add("active");
-}
+	modal.classList.add("active");
+	overlay.classList.add("active");
+};
 
 closeBtn.onclick = () => {
-    modal.classList.remove("active"); 
-    overlay.classList.remove("active");
-}
+	modal.classList.remove("active");
+	overlay.classList.remove("active");
+};
+
+overlay.onclick = () => {
+	modal.classList.remove("active");
+	overlay.classList.remove("active");
+};
+
+document.addEventListener("keydown", (event) => {
+	if (event.key === "Escape") {
+		modal.classList.remove("active");
+		overlay.classList.remove("active");
+	}
+});
