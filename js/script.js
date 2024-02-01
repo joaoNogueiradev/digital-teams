a0d6c9fd7f5f297f7f229c449a80cfb56ccc6502
joaoNogueiraDev <devjoaoluiz@gmail.com>
2024-02-01 10:49:54 -0300

    command lines debug

--- js/script.js
+++ js/script.js
@@ -32,13 +32,19 @@ document.addEventListener("keydown", function(event) {
 modal.onsubmit = () => {
     event.preventDefault();
     
-    // console.log(nome.value+' '+capacidade.value);
+    let capacidadeNumber = Number(capacidade.value);
+
+    if(isNaN(capacidadeTeam)){
+        alert("Favor inserir uma quantidade válida:");
+        capacidade.value="";
+        return;
+    }
 
     listTeams.innerHTML = '';
     listTeams.innerHTML = `
         <li>
-            <h4>Nome do team <box-icon name='show'></box-icon></h4>
-            <h1>0 <span>/ 0</span></h1>
+            <h4>${nome.value}<box-icon name='show'></box-icon></h4>
+            <h1>0 <span>/ ${capacidadeNumber}</span></h1>
             <div class="actions">
                 <button>adicionar</button>
                 <button><box-icon name="trash"></box-icon></button>
@@ -52,3 +58,4 @@ modal.onsubmit = () => {
 
 // evento específico para form é o onsubmit
 // event.preventDefault(); previne que o form procure arquivos e faça o que foi comandado na função
+// `` são conhecidas na programação como template literals
