891e5ac14a5b980f5def7b1f5a57f8a0b7a7f1bb
João Luiz Lima Nogueira <devjoaoluiz@gmail.com>
2024-02-16 21:39:28 -0300

    add cards for the client and validation on name value

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let teams = [];
+
 createBtn.onclick = () => {
 	modal.classList.add("active");
 	overlay.classList.add("active");
@@ -13,15 +15,8 @@ overlay.onclick = () => {
 	overlay.classList.remove("active");
 };
 
-// fechanddo modal no Esc com função callback
-// document.addEventListener("keydown", (event) => {
-// 	if (event.key === "Escape") {
-// 		modal.classList.remove("active");
-// 		overlay.classList.remove("active");
-// 	}
-// });
 
-document.addEventListener("keydown", function (event) {
+document.addEventListener("keydown", (event) => {
 	if (event.key === "Escape") {
 		modal.classList.remove("active");
 		overlay.classList.remove("active");
@@ -29,33 +24,59 @@ document.addEventListener("keydown", function (event) {
 });
 
 modal.onsubmit = () => {
-    event.preventDefault();
+	event.preventDefault();
 
-	let nomeTeam = nome.value;
-	let capacidadeTeam = Number(capacidade.value);
-
-	if (isNaN(capacidadeTeam)) {
+	if (isNaN(capacidade.value)) {
 		alert("Por favor, digite um número válido para a capacidade.");
 		capacidade.value = "";
 		return; // Interrompe a execução do código se a entrada não for um número
 	}
 
-	listTeams.innerHTML = "";
-	listTeams.innerHTML = `
-        <li>
-            <h4>${nome.value}<box-icon name='show'></box-icon></h4>
-            <h1>0 <span>/ ${capacidadeNumber}</span></h1>
-            <div class="actions">
-                <button>adicionar</button>
-                <button><box-icon name="trash"></box-icon></button>
-            </div>
-        </li>
-    `;
+	let newTeamName = nome.value;
+
+	for (let i = 0; i < teams.length; i++) {
+		if (teams[i].name === newTeamName) {
+			if (confirm("Já existe um time com esse nome, gostaria de repetir a criação?")) {
+			} else {
+				alert("Criação cancelada!");
+				nome.value = '';
+				return;
+			}
+		}
+	}
+
+	teams.push({
+		name: newTeamName,
+		capacity: capacidade.value,
+		members: [],
+	});
+	
+	nome.value = '';
+	capacidade.value = '';
+
+	let newTeam = ``;
+
+	for (let i = 0; i < teams.length; i++) {
+		newTeam +=`
+			<li>
+			<h4>${teams[i].name}<box-icon name='show'></box-icon></h4>
+			<h1>0 <span>/ ${teams[i].capacity}</span></h1>
+			<div class="actions">
+			<button>adicionar</button>
+			<button><box-icon name="trash"></box-icon></button>
+			</div>
+			</li>
+		`;
+
+		listTeams.innerHTML = newTeam;
+	}
+	
 	modal.classList.remove("active");
 	overlay.classList.remove("active");
 };
 
-
 // evento específico para form é o onsubmit
 // event.preventDefault(); previne que o form procure arquivos e faça o que foi comandado na função
 // `` são conhecidas na programação como template literals
+// sempre que precisar de uma
+// quando precisar iterar elementos (Sem usar element create e appendChild), utilizar variáveis para que o novo elemento seja colocado e usaddo em iteração
